5a0a470e0fb11fb4f352630b55bf01145cd32598
MateenCPH <cph-mr645@cphbusiness.dk>
2024-02-01 14:58:58 +0100

    Method fizzbuzz

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,8 +10,16 @@ public class Main {
 
         System.out.println("Pantheon mid");
 
-        for (int i = 0; i < 201; i++){
-            System.out.println(i);
+        for (int i = 1; i < 201; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 5 == 0){
+                System.out.println("Buzz");
+            } else if (i % 3 == 0){
+                System.out.println("Fizz");
+            } else {
+                System.out.println(i);
+            }
         }
     }
 }
