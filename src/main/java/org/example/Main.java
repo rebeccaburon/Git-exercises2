ae52c9aef8a7340237fc977f14d01c76267fdbf1
Marcus Rasmussen <marcusphr1808@gmail.com>
2024-02-01 09:39:04 +0100

    FizzBuzz printout

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,5 +6,17 @@ public class Main {
         System.out.println("Hello world!");
 
         System.out.println("Mateen er noob");
+
+        for (int i = 1 ; i <= 100 ; i++){
+            if (i % 3 == 0 && i % 5 == 0){
+                System.out.println("FizzBuzz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else {
+                System.out.println(i);
+            }
+        }
     }
 }
