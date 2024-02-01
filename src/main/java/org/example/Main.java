6cf55adf7d5652dfa7fde68933a88473aa1c0e99
Rodney <cph-rm135@cphbusiness.dk>
2024-02-01 21:22:23 +0100

    BuzzFizz instead of FizzBuzz

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
                 for (int i = 1; i <= 200; i++) {
                     if (i % 3 == 0 && i % 5 == 0) {
-                        System.out.println("FizzBuzz");
+                        System.out.println("BuzzFizz");
                     } else if (i % 3 == 0) {
                         System.out.println("Fizz");
                     } else if (i % 5 == 0) {
