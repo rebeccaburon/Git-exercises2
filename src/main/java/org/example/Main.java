59761e94aa629058a9b4204cc026f6ed5b328c1f
Rodney <cph-rm135@cphbusiness.dk>
2024-02-01 21:11:37 +0100

    countto 200

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-                for (int i = 1; i <= 100; i++) {
+                for (int i = 1; i <= 200; i++) {
                     if (i % 3 == 0 && i % 5 == 0) {
                         System.out.println("FizzBuzz");
                     } else if (i % 3 == 0) {
